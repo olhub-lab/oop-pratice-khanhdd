1261931afb6d2b18cbda1cce568d562b1674f908
khanh <dkhanh0512@gmail.com>
2026-04-13 00:13:10 +0700

    init develop

--- src/main/java/config/DatabaseConnectionProvider.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DatabaseConnectionProvider {
-  Connection getConnection() throws SQLException;
-  void closePool();
-}
--- src/main/java/model/enums/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package model.enums;
-
-public enum OrderStatus {
-  PENDING,
-  SUCCESS,
-  FAILED,
-  CANCELLED
-}
